a8a44fe6b6e75cb6dcc72f8f9e8e049363a372d5
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2012-03-18 02:43:30 -0500

    make drink creator work

--- lib/DU/App/Command/drink/new.pm
+++ lib/DU/App/Command/drink/new.pm
@@ -13,14 +13,27 @@ sub usage_desc { 'du drink new $drink' }
 sub execute {
    my ($self, $opt, $args) = @_;
 
-   my $i = $self->app->app->schema->resultset('Ingredient')->create(
+   my $i = $self->app->app->schema->create_drink(
       DU::Util::edit_data({
-         name => 'new ingredient',
-         description => undef,
+         description => 'Refreshing beverage for a hot day',
+         name => 'Tom Collins',
+         ingredients => [{
+            name => 'Club Soda',
+            volume => 1,
+         }, {
+            name => 'Gin',
+            volume => .5,
+         }, {
+            name => 'Lemon Juice',
+            volume => .25,
+         }, {
+            name => 'Simple Syrup',
+            volume => 1 / 24,
+         }],
       })
    );
 
-   say 'ingredient (' . $i->name . ') created';
+   say 'drink (' . $i->name . ') created';
 }
 
 1;
