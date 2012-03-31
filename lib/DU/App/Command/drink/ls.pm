593947ecb90d184fbc8d8ce902fb3b632bcd58b5
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2012-03-30 23:29:58 -0500

    SEE EMAIL TO RIBA

--- lib/DU/App/Command/drink/ls.pm
+++ lib/DU/App/Command/drink/ls.pm
@@ -10,11 +10,24 @@ sub abstract { 'list drinks' }
 
 sub usage_desc { 'du drink ls' }
 
+sub opt_spec {
+   [ 'some_ingredients|s',  'only drinks I can at least partially make' ],
+   [ 'no_ingredients|n',  'only drinks I cannot make at all' ],
+}
+
 sub execute {
    my ($self, $opt, $args) = @_;
 
-   print drink_as_markdown($_)
-     for $self->app->app->schema->resultset('Drink')->all
+
+   my $rs = $self->app->app->schema->resultset('Drink');
+   my $user = $self->app->app->schema->resultset('User')->search({
+      name => 'frew',
+   })->single;
+
+   $rs = $rs->some($user) if $opt->some_ingredients;
+   $rs = $rs->none($user) if $opt->no_ingredients;
+
+   print drink_as_markdown($_) for $rs->all
 }
 
 1;
--- lib/DU/Schema/ResultSet/Drink.pm
+++ lib/DU/Schema/ResultSet/Drink.pm
@@ -13,4 +13,53 @@ sub cli_find {
    })
 }
 
+# one of my ingredients is used
+sub some {
+   my ($self, $user) = @_;
+
+   my $ids = $self->search({
+      'ingredient.id' => {
+         -in => $user->inventory_items->get_column('ingredient_id')->as_query
+      },
+   }, {
+      join => { links_to_drink_ingredients => 'ingredient' },
+      group_by => 'me.id',
+   })->get_column('id')->as_query;
+
+   $self->search({ id => { -in => $ids } })
+}
+
+# none of my ingredients are used
+sub none {
+   my ($self, $user) = @_;
+
+   #my $ii = $user->inventory_items;
+
+   my $ii = $self->result_source->schema->resultset('InventoryItem')
+      ->search({
+         'ii.user_id' => $user->id
+      }, {
+         alias => 'ii',
+      });
+
+   my $q = $ii
+      ->related_resultset('ingredient')
+      ->related_resultset('links_to_drink_ingredients')
+      ->related_resultset('drink')
+      ->search({
+         'drink.id' => { -ident => 'me.id' },
+      }, {
+         #alias => 'ud',
+      })
+      ->as_query;
+
+   my $ids = $self->search({
+      -not_exists => $q,
+   }, {
+      group_by => 'me.id',
+   })->get_column('id')->as_query;
+
+   $self->search({ id => { -in => $ids } })
+}
+
 1;
