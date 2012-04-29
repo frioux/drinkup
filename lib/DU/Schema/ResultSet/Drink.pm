faadc7c315a28c024c33fb4a98639a79354fa9ff
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2012-04-29 01:14:49 -0500

    factor ineq out of nearly and document drink rs methods

--- lib/DU/Schema/ResultSet/Drink.pm
+++ lib/DU/Schema/ResultSet/Drink.pm
@@ -13,7 +13,6 @@ sub cli_find {
    })
 }
 
-# one of my ingredients is used
 sub some {
    my ($self, $user) = @_;
 
@@ -29,7 +28,6 @@ sub some {
    $self->search({ id => { -in => $ids } })
 }
 
-# none of my ingredients are used
 sub none {
    my ($self, $user) = @_;
 
@@ -63,8 +61,8 @@ sub none {
    $self->search({ id => { -in => $ids } })
 }
 
-sub nearly {
-   my ($self, $user, $less) = @_;
+sub ineq {
+   my ($self, $user, $min, $max) = @_;
 
    my $ingredients_on_hand = $self->result_source->schema->resultset('InventoryItem')
       ->search({
@@ -98,13 +96,58 @@ sub nearly {
          $ioh_sql ii
       )
       WHERE di.drink_id = ii.drink_id AND
-            di.ingredient_count = ii.ingredient_count + ?
+            di.ingredient_count >= ii.ingredient_count + ? AND
+            di.ingredient_count <= ii.ingredient_count + ?
 SQL
-   @ri_bind, @ioh_bind, [ { sqlt_datatype => 'int' } => $less ] ];
+   @ri_bind, @ioh_bind, [{ sqlt_datatype => 'int' } => $min], [{ sqlt_datatype => 'int' } => $max] ];
 
    $self->search({ 'me.id' => { -in => $creation } });
 }
 
+sub nearly { $_[0]->ineq($_[1], $_[2], $_[2]); }
+
 sub every { $_[0]->nearly($_[1], 0) }
 
 1;
+
+__END__
+
+=pod
+
+=head1 METHODS
+
+=head2 cli_find
+
+ $rs->cli_find('lem*');
+
+Drinks with a name that L<matches|DU::Schema::ResultSet/_glob_to_like>
+the passed string.
+
+=head2 some
+
+ $rs->some($user)
+
+Drinks that the passed user has at least one ingredient for
+
+=head2 none
+
+ $rs->none($user)
+
+Drinks that the passed user has no ingredients for
+
+=head2 ineq
+
+ $rs->ineq($user, 1, 3)
+
+Drinks that the passed user could make if he bought $x to $y ingredients,
+inclusive.
+
+=head2 nearly
+
+ $rs->nearly($user, 2)
+
+Drinks that the passed user could make if he bought exactly $x ingredients
+
+=head2 every
+
+Drinks that the passed user can make given his current inventory
