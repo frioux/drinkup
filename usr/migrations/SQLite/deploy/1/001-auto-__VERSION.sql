dade1725edec6b705fc17e11d7e57b257b2dde3f
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2012-09-18 20:45:10 -0500

    initial migration

---
+++ usr/migrations/SQLite/deploy/1/001-auto-__VERSION.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "dbix_class_deploymenthandler_versions" (
+  "id" INTEGER PRIMARY KEY NOT NULL,
+  "version" varchar(50) NOT NULL,
+  "ddl" text,
+  "upgrade_sql" text
+);
+CREATE UNIQUE INDEX "dbix_class_deploymenthandler_versions_version" ON "dbix_class_deploymenthandler_versions" ("version")
---
+++ usr/migrations/_common/deploy/1/010-init.pl
@@ -0,0 +1,21 @@
+#!perl
+
+use 5.16.1;
+use warnings;
+
+use DBIx::Class::DeploymentHandler::DeployMethod::SQL::Translator::ScriptHelpers 'schema_from_schema_loader';
+
+schema_from_schema_loader({ naming => 'v7' }, sub {
+   my ($schema, $version_set) = @_;
+
+   $schema->resultset('Unit')->populate([
+      [qw(name gills)],
+      [ounce      => 1 / 4         ] ,
+      [tablespoon => 1 / 4 / 2     ] ,
+      [teaspoon   => 1 / 4 / 2 / 3 ] ,
+      [dash       => undef         ] ,
+   ]);
+
+   $schema->resultset('User')->create({ name => 'frew' });
+});
+
